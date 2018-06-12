c808fa533cdc3d0914b311b9d0df1052ec2eb119
Tao Du <taodu@csail.mit.edu>
2018-06-11 20:46:29 -0700

    About to change the reward function in inverted double pendulum.

--- baselines/pareto_env_wrapper.py
+++ baselines/pareto_env_wrapper.py
@@ -1,5 +1,7 @@
 from gym.envs.classic_control import CartPoleEnv
 from gym.envs.mujoco import SwimmerEnv
+from gym.envs.mujoco import InvertedPendulumEnv
+from gym.envs.mujoco import InvertedDoublePendulumEnv
 
 class CartPoleParetoWrapper(CartPoleEnv):
     env = None
@@ -26,6 +28,50 @@ class CartPoleParetoWrapper(CartPoleEnv):
 
     def render(self, mode='human'):
         return self.env.render(mode)
+
+
+class InvertedPendulumParetoWrapper(InvertedPendulumEnv):
+    env = None
+    
+    def __init__(self, env):
+        self.env = env
+        self.action_space = env.action_space
+        self.observation_space = env.observation_space
+        self.reward_range = env.reward_range
+        self.metadata = env.metadata
+
+    def step(self, action):
+        ob, reward, done, info = self.env.step(action)
+        # TODO: Recalculate reward.
+        return ob, reward, done, info
+
+    def reset_model(self):
+        return self.env.reset_model()
+
+    def viewer_setup(self):
+        return self.env.viewer_setup()
+
+
+class InvertedDoublePendulumParetoWrapper(InvertedDoublePendulumEnv):
+    env = None
+
+    def __init__(self, env):
+        self.env = env
+        self.action_space = env.action_space
+        self.observation_space = env.observation_space
+        self.reward_range = env.reward_range
+        self.metadata = env.metadata
+
+    def step(self, action):
+        ob, reward, done, info = self.env.step(action)
+        # TODO: Recalculate reward.
+        return ob, reward, done, info
+
+    def reset(self):
+        return self.env.reset()
+
+    def render(self, mode='human'):
+        self.env.render(mode)
 
 class SwimmerParetoWrapper(SwimmerEnv):
     env = None
